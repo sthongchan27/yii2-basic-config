73ece003e1467f27580cbe12ae09ef26f69694e7
sthongchan27 <sthongchan27@gmail.com>
2024-03-03 13:44:36 +0700

    ----e

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'dev',
-    'password' => 'password',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
